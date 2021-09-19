872ea446ed93f822ee8a0d78b1c8b4202a9e1201
soumendrak <soumendra.s@outlook.com>
2021-09-19 20:47:41 +0530

    arguments added to release.sh

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 python setup.py sdist bdist_wheel
 twine check dist/*
-bumpversion --current-version 0.0.0 patch setup.py openodia/__init__.py
+bumpversion --current-version $1 $2 setup.py openodia/__init__.py
 twine upload dist/*
