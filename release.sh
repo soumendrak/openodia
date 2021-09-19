8d7f5340d97026e12504ecbff034979ab420f491
soumendrak <soumendra.s@outlook.com>
2021-09-19 22:18:31 +0530

    minor changes

--- release.sh
+++ release.sh
@@ -1,7 +1,8 @@
 # $1 is the current version, for e.g. 0.0.5
 # $2 is the release severity major.minor.patch
-bumpversion --current-version $1 $2 setup.py openodia/__init__.py
-git commit -am "version upgraded" && git push
-python setup.py sdist bdist_wheel
-twine check dist/*
+bumpversion --current-version $1 $2 setup.py openodia/__init__.py 
+git commit -am "version upgraded" 
+git push 
+python setup.py sdist bdist_wheel 
+twine check dist/* 
 twine upload --skip-existing dist/*
