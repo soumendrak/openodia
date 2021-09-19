8be723d97ebd0602d884071c688e0325c91df67d
soumendrak <soumendra.s@outlook.com>
2021-09-19 20:51:41 +0530

    version upgraded

--- release.sh
+++ release.sh
@@ -2,4 +2,4 @@ python setup.py sdist bdist_wheel
 twine check dist/*
 bumpversion --current-version $1 $2 setup.py openodia/__init__.py
 git commit -am "version upgraded" && git push
-twine upload dist/*
+twine upload --skip-existing dist/*
