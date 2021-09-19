434a9806d96cb00f6f7f3103f228404c9ae4deb0
soumendrak <soumendra.s@outlook.com>
2021-09-19 20:49:36 +0530

    git added to release.sh

--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
 python setup.py sdist bdist_wheel
 twine check dist/*
 bumpversion --current-version $1 $2 setup.py openodia/__init__.py
+git commit -am "version upgraded" && git push
 twine upload dist/*
