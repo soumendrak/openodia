2a396778a7a8b1838b44aab95cfa2425dadce0fa
soumendrak <soumendra.s@outlook.com>
2021-10-06 13:34:08 +0530

    blacked

--- openodia/corpus/dictionary.py
+++ openodia/corpus/dictionary.py
@@ -7,7 +7,9 @@ import json
 from typing import Dict
 
 
-def get_dictionary(file_location: str = "openodia/corpus/En-Or_word_pairs_v2.json") -> Dict[str, str]:
+def get_dictionary(
+    file_location: str = "openodia/corpus/En-Or_word_pairs_v2.json",
+) -> Dict[str, str]:
     with open(file_location) as dh:
         dictionary_data = json.load(dh)
         return dictionary_data
