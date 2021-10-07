a5533c0d818525fa273eaffd5b615a64f98929d5
soumendrak <soumendra.s@outlook.com>
2021-10-07 15:32:18 +0530

    minor changes

--- openodia/corpus/dictionary.py
+++ openodia/corpus/dictionary.py
@@ -8,6 +8,7 @@ import os
 from typing import Dict
 from openodia.common.utility import LOGGER
 
+
 def get_dictionary() -> Dict[str, str]:
     dict_file = os.path.join(os.path.dirname(__file__), "En-Or_word_pairs_v2.json")
     LOGGER.info(f"Getting offline dictionary data from: {dict_file}")
