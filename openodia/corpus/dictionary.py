80e5867abbe18d92ce2699ec425d728193047b84
soumendrak <proud_odia@outlook.com>
2021-10-07 22:46:21 +0530

    INFO --> DEBUG

--- openodia/corpus/dictionary.py
+++ openodia/corpus/dictionary.py
@@ -11,7 +11,7 @@ from openodia.common.utility import LOGGER
 
 def get_dictionary() -> Dict[str, str]:
     dict_file = os.path.join(os.path.dirname(__file__), "En-Or_word_pairs_v2.json")
-    LOGGER.info(f"Getting offline dictionary data from: {dict_file}")
+    LOGGER.debug(f"Getting offline dictionary data from: {dict_file}")
     with open(dict_file, encoding="utf8") as dh:
         dictionary_data = json.load(dh)
         return dictionary_data
