ccd2b32685c353c840dc4e37ae14026b41068b7a
soumendrak <proud_odia@outlook.com>
2021-10-14 16:56:23 +0530

    Minor changes on standard

--- openodia/__init__.py
+++ openodia/__init__.py
@@ -2,11 +2,12 @@
 """
 __version__ = "0.1.11"
 
+from .common.constants import STOPWORDS
 from ._letters import Letters as alphabet
 from ._odianames import Names as name
-from ._understandData import UnderstandData as ud
-from ._translate import odia_to_other_lang, other_lang_to_odia, universal_translation
 from ._summarization import WordFrequency
+from ._translate import odia_to_other_lang, other_lang_to_odia, universal_translation
+from ._understandData import UnderstandData as ud
 
 __all__ = [
     "alphabet",
--- openodia/corpus/dictionary.py
+++ openodia/corpus/dictionary.py
@@ -10,8 +10,9 @@ from openodia.common.utility import LOGGER
 
 
 def get_dictionary() -> Dict[str, str]:
+    """Get the dictionary by reading the dictionary corpus"""
     dict_file = os.path.join(os.path.dirname(__file__), "En-Or_word_pairs_v2.json")
     LOGGER.debug(f"Getting offline dictionary data from: {dict_file}")
-    with open(dict_file, encoding="utf8") as dh:
+    with open(dict_file, mode="rt", encoding="utf-8") as dh:
         dictionary_data = json.load(dh)
         return dictionary_data
